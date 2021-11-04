5fd43037f3cd5049195953d565c1a6e73e5c83c4
gusjer <gusjer@gmail.com>
2021-11-04 20:37:40 +0100

    feat: point backend to correct

--- main.tf
+++ main.tf
@@ -10,12 +10,12 @@ terraform {
     }
   }
   required_version = "~> 1.0"
-
+  
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "Gusjer-Demos"
 
     workspaces {
-      name = "REPLACE_ME"
+      name = "demo-github-actions"
     }
   }
 }
